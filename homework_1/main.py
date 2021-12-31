e36731bac6a657751f1cf6f2e8ac4b86761861a6
Evgen <rog.been@yandex.ru>
2021-12-31 20:17:26 +0700

    r

---
+++ homework_1/main.py
@@ -0,0 +1,22 @@
+print('hello_world')#
+print(7-2)
+print(0.2+0.5)
+print(10*2)
+print(23/2)
+print(23//2)
+print(7%2)
+print(1.56*6.35)
+print(type(10/2))
+if 5 > 3:
+    print('Yes')
+else:
+    print ('No')
+s = 12
+if 12 > s:
+    print('12 больше ')
+elif 12 == s:
+    print('значения  равны')
+    a = 'null'
+    b = 'something'
+s=str(a+b)
+print(s)
