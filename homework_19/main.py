00156d9bc3966c78fa7082fbf8510af01e8b0943
Evgen <rog.been@yandex.ru>
2022-02-15 14:59:12 +0700

    r

--- homework_19/main.py
+++ homework_19/main.py
@@ -1,129 +1,57 @@
 from sqlalchemy.orm import (
-Session as SessionType,
-scoped_session,
-sessionmaker,
+    Session as SessionType,
+    scoped_session,
+    sessionmaker,
 )
 from models.author import Author
 from models.book import Book
 from models.base import engine
+
 session_factory = sessionmaker(bind=engine)
 Session = scoped_session(session_factory)
-def create_author(session: SessionType,Name_author:str)-> Author:
-    """""
+
+
+def create_author(session: SessionType, Name_author: str) -> Author:
+    """
     :param session:
     :param Name_author:
     :return:
-    """""
+    """
     author = Author(Name_author=Name_author)
     print("create author", author)
     session.add(author)
     session.commit()
     print("saved author", author)
     return author
-def main():
+
+
+def create_book(session: SessionType, Name_books: str, author_id: int) -> Book:
     """""
+    :param session:
+    :param Name_books:
+    :param author_id:
     :return:
     """""
+    book = Book(Name_books=Name_books, author_id=author_id)
+    print("create book", book)
+    session.add(book)
+    session.commit()
+    print("saved book", book)
+    return book
+
+
+def main():
+    """
+    :return:
+    """
     session = Session()
     create_author(session, "Author number one")
     create_author(session, "Author number two")
-if __name__=='main':
+    create_book(session, "Thomas More.Utopia",1)
+    create_book(session, "The Art of War",1)
+    create_book(session, "Art and Beauty in Medieval Aesthetics", 1)
+    create_book(session, "What do women want",2)
+
+if __name__ == '__main__':
     main()
-
-
-
-
-# def create_book(session: SessionType, Name_books:str,author_id:int)-> Book:
-#     """""
-#     :param session:
-#     :param Name_books:
-#     :param author_id:
-#     :return:
-#     """""
-#     book = Book(Name_books=Name_books,author_id=author_id)
-#     print("create book", book)
-#     session.add(book)
-#     session.commit()
-#     print("saved book", book)
-#     return book
-
-
-
-
-# def create_author_and_books(
-#         session:SessionType,
-#         *book_Name_books:str,
-# )-> list[Book]:
-#     """""
-#     :param session:
-#     :param Name_books:
-#     :return:
-#     """""
-#     author = session.query(Author)
-#     for book_name in book_Name_books:
-#         book = Book(Name_books=book_name, author=author)
-#         print("create book",book)
-#         session.add(book)
-#     session.commit()
-#     print("author's books", author.books)
-#     return author.books
-
-
-# def main():
-#     """""
-#         :return
-#     """""
-#     session:SessionType= Session()
-#
-#     create_author_and_books(
-#         session,
-#         "Author number one",
-#         "Thomas More.Utopia",
-#         "The Art of War"
-#     )
-#     create_author_and_books(
-#         session,
-#         "Author number two",
-#         "What do women want"
-#     )
-#     session.close()
-#
-# if __name__=='main':
-#     main()
-#
-#
-#
-#
-#
-#
-#
-#
-#
-
-
-# def main():
-#     """""
-#     :return:
-#     """""
-# session = Session()
-#
-# session: SessionType = Session()
-# aut_one=create_author(session,"aut_one")
-# another_author_one =session.get(Author,aut_one.id)
-# print("another_author_one",another_author_one)
-# aut_two=create_author(session,"aut_two")
-# another_author_two =session.get(Author,aut_two.id)
-# print("another_author_two",another_author_two)
-
-# session = Session()
-# create_author(session, "Author number one")
-# create_author(session, "Author number two")
-#
-# create_book(session, "Thomas More.Utopia")
-# create_book(session, "The Art of War")
-# create_book(session, "What do women want")
-
-
-
-
 
